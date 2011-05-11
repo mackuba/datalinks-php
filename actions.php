d8eb344e596d3313b54f943f325322b174b21745
Jakub Suder <jakub.suder@gmail.com>
2011-05-11 20:35:07 +0200

    changed license to MIT

--- actions.php
+++ actions.php
@@ -1,12 +1,8 @@
 <?PHP
-/****************************************************************************
- *   Copyright by Jakub Suder                                               *
- *                                                                          *
- *   This program is free software; you can redistribute it and/or modify   *
- *   it under the terms of the GNU General Public License as published by   *
- *   the Free Software Foundation; either version 2 of the License, or      *
- *   (at your option) any later version.                                    *
- ****************************************************************************/
+// -------------------------------------------------------
+// Copyright (c) 2007 Jakub Suder <jakub.suder@gmail.com>
+// Licensed under MIT license
+// -------------------------------------------------------
 
 /* Auxilliary function for most actions, which updates all "ancestors" of a given category with the specified UPDATE sql query. */
 function updateAncestors($chlinks, $chcats, $chvlinks, $chvcats, $categoryId) {
--- javascripts/datalinks.js
+++ javascripts/datalinks.js
@@ -1,11 +1,7 @@
-/****************************************************************************
- *   Copyright by Jakub Suder                                               *
- *                                                                          *
- *   This program is free software; you can redistribute it and/or modify   *
- *   it under the terms of the GNU General Public License as published by   *
- *   the Free Software Foundation; either version 2 of the License, or      *
- *   (at your option) any later version.                                    *
- ****************************************************************************/
+// -------------------------------------------------------
+// Copyright (c) 2007 Jakub Suder <jakub.suder@gmail.com>
+// Licensed under MIT license
+// -------------------------------------------------------
 
 // global vars
 var scriptName;
